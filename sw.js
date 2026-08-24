d3a23cf38b8486e6f87e97062e0f1337e55848dc
fabie250 <73535342+fabie250@users.noreply.github.com>
2026-08-25 02:15:11 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'siheng-v1';
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
